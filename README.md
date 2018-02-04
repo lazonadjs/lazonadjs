2b1ae41a7c152818065a175c04f3c9e41d2cdef2
Lincoln Ward <linwilvic@yahoo.com>
2018-02-04 13:57:38 -0500

    Add existing file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wlojii
+Wlojii is a global, experiential, Liberian Music platform built for fans, directly from Liberian artists around the world. Wlojii members enjoy exclusively curated content that directly connect artists with their fans in multiple ways. The Platform offers high-fidelity, CD sound quality music, an opportunity to discover new artists and new Liberian music. Login, listen to available playlists or create your own. Be a part of The Wlojii Community.
---
+++ www/js/app.js
@@ -0,0 +1,212 @@
+angular.module("wlojii", ["ngCordova","ionic","ionMdInput","ionic-material","ion-datetime-picker","ionic.rating","utf8-base64","angular-md5","chart.js","pascalprecht.translate","tmh.dynamicLocale","ionicLazyLoad","wlojii.controllers", "wlojii.services"])
+	.run(function($ionicPlatform,$window,$interval,$timeout,$ionicHistory,$ionicPopup,$state,$rootScope){
+
+		$rootScope.appName = "wlojii" ;
+		$rootScope.appLogo = "data/images/header/logo.png" ;
+		$rootScope.appVersion = "2.1" ;
+		$rootScope.headerShrink = false ;
+
+		$ionicPlatform.ready(function() {
+			//required: cordova plugin add ionic-plugin-keyboard --save
+			if(window.cordova && window.cordova.plugins.Keyboard) {
+				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+				cordova.plugins.Keyboard.disableScroll(true);
+			}
+
+			//required: cordova plugin add cordova-plugin-statusbar --save
+			if(window.StatusBar) {
+				StatusBar.styleDefault();
+			}
+
+
+			//required: cordova plugin add onesignal-cordova-plugin --save
+			if(window.plugins && window.plugins.OneSignal){
+				window.plugins.OneSignal.enableNotificationsWhenActive(true);
+				var notificationOpenedCallback = function(jsonData){
+					try {
+						$timeout(function(){
+							$window.location = "#/wlojii/" + jsonData.notification.payload.additionalData.page ;
+						},200);
+					} catch(e){
+						console.log("onesignal:" + e);
+					}
+				}
+				window.plugins.OneSignal.startInit("22b0e384-d442-427f-a619-bc82cf6d11b5").handleNotificationOpened(notificationOpenedCallback).endInit();
+			}    
+
+
+		});
+		$ionicPlatform.registerBackButtonAction(function (e){
+			if($ionicHistory.backView()){
+				$ionicHistory.goBack();
+			}else{
+				$state.go("wlojii.dashboard");
+			}
+			e.preventDefault();
+			return false;
+		},101);
+	})
+
+
+	.filter("to_trusted", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+
+	.filter("trustUrl", function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	})
+
+	.filter("trustJs", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsJs(text);
+		};
+	}])
+
+	.filter("strExplode", function() {
+		return function($string,$delimiter) {
+			if(!$string.length ) return;
+			var $_delimiter = $delimiter || "|";
+			return $string.split($_delimiter);
+		};
+	})
+
+	.filter("strDate", function(){
+		return function (input) {
+			return new Date(input);
+		}
+	})
+	.filter("strHTML", ["$sce", function($sce){
+		return function(text) {
+			return $sce.trustAsHtml(text);
+		};
+	}])
+	.filter("strEscape",function(){
+		return window.encodeURIComponent;
+	})
+	.filter("strUnscape", ["$sce", function($sce) {
+		var div = document.createElement("div");
+		return function(text) {
+			div.innerHTML = text;
+			return $sce.trustAsHtml(div.textContent);
+		};
+	}])
+
+	.filter("objLabel", function(){
+		return function (obj) {
+			var new_item = [];
+			angular.forEach(obj, function(child) {
+				new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v,l) {
+					if (indeks !== 0) {
+					new_item.push(l);
+				}
+				indeks++;
+				});
+			});
+			return new_item;
+		}
+	})
+	.filter("objArray", function(){
+		return function (obj) {
+			var new_items = [];
+			angular.forEach(obj, function(child) {
+				var new_item = [];
+				var indeks = 0;
+				angular.forEach(child, function(v){
+						if (indeks !== 0){
+							new_item.push(v);
+						}
+						indeks++;
+					});
+					new_items.push(new_item);
+				});
+			return new_items;
+		}
+	})
+
+
+.config(["$translateProvider", function ($translateProvider){
+	$translateProvider.preferredLanguage("en-us");
+	$translateProvider.useStaticFilesLoader({
+		prefix: "translations/",
+		suffix: ".json"
+	});
+}])
+
+
+.config(function(tmhDynamicLocaleProvider){
+	tmhDynamicLocaleProvider.localeLocationPattern("lib/ionic/js/i18n/angular-locale_{{locale}}.js");
+	tmhDynamicLocaleProvider.defaultLocale("en-us");
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider,$httpProvider,$ionicConfigProvider){
+	try{
+		// Domain Whitelist
+		$sceDelegateProvider.resourceUrlWhitelist([
+			"self",
+			new RegExp('^(http[s]?):\/\/(w{3}.)?youtube\.com/.+$'),
+			new RegExp('^(http[s]?):\/\/(w{3}.)?w3schools\.com/.+$'),
+		]);
+	}catch(err){
+		console.log("%cerror: %cdomain whitelist","color:blue;font-size:16px;","color:red;font-size:16px;");
+	}
+	$stateProvider
+	.state("wlojii",{
+		url: "/wlojii",
+			abstract: true,
+			templateUrl: "templates/wlojii-side_menus.html",
+			controller: "side_menusCtrl",
+	})
+
+	.state("wlojii.about_us", {
+		url: "/about_us",
+		views: {
+			"wlojii-side_menus" : {
+						templateUrl:"templates/wlojii-about_us.html",
+						controller: "about_usCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("wlojii.dashboard", {
+		url: "/dashboard",
+		cache:false,
+		views: {
+			"wlojii-side_menus" : {
+						templateUrl:"templates/wlojii-dashboard.html",
+						controller: "dashboardCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+	.state("wlojii.slide_tab_menu", {
+		url: "/slide_tab_menu",
+		views: {
+			"wlojii-side_menus" : {
+						templateUrl:"templates/wlojii-slide_tab_menu.html",
+						controller: "slide_tab_menuCtrl"
+					},
+			"fabButtonUp" : {
+						template: '',
+					},
+		}
+	})
+
+
+// router by user
+
+
+	$urlRouterProvider.otherwise("/wlojii/dashboard");
+});
